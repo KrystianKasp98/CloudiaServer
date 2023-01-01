9511cba293121cb45613d2262cbc743c22a97c2e
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-01-02 00:25:55 +0100

    add test for users

--- src/routes/Users/users.test.ts
+++ src/routes/Users/users.test.ts
@@ -5,7 +5,17 @@ import {ExpressResult} from '../../types';
 import {statusCode} from '../../utils/consts';
 import {validateBody, compareResultAndExpect} from '../../utils/helpers';
 
+const userCredentials = {
+  login: 'CloudIa202',
+  password: 'fiufiu202'
+}
+
 describe('/users route [SUCCESS]', () => {
-  it('[POST] login');
+  it('[POST] /login', async () => {
+    const res: ExpressResult = await request(app).post('/users/login').send(userCredentials);
+
+    expect(res.statusCode).toEqual(statusCode.ok);
+    expect(res.body).toEqual(true);
+  });
 });
 
